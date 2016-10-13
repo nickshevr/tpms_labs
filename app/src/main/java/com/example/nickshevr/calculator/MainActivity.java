341eba22d283be7dea63fad7acd37bfdee6bf4f3
nickshevr <nickshevr@gmail.com>
2016-10-13 16:49:26 +0300

    Added calc memory

--- app/src/main/java/com/example/nickshevr/calculator/MainActivity.java
+++ app/src/main/java/com/example/nickshevr/calculator/MainActivity.java
@@ -2,18 +2,20 @@ package com.example.nickshevr.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
-import android.content.Intent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.util.Stack;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String LOG_TAG = "FirstActivity";
+
+    private static Stack<Double> memory = new Stack<Double>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +26,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ImageView multiplicationImageButton = (ImageView) findViewById(R.id.multiplication);
         ImageView substractionImageButton = (ImageView) findViewById(R.id.subtract);
         ImageView additionImageButton = (ImageView) findViewById(R.id.addition);
+        ImageView mraddImageButton = (ImageView) findViewById(R.id.mradd);
+        ImageView mrremoveImageButton = (ImageView) findViewById(R.id.mrremove);
 
         multiplicationImageButton.setOnClickListener(this);
         substractionImageButton.setOnClickListener(this);
         additionImageButton.setOnClickListener(this);
         divisionImageButton.setOnClickListener(this);
+        mrremoveImageButton.setOnClickListener(this);
+        mraddImageButton.setOnClickListener(this);
     }
 
 
@@ -47,21 +53,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.addition:
                 addition();
                 break;
+            case R.id.mrremove:
+                popMemory();
+                break;
+            case R.id.mradd:
+                addMemory();
+                break;
         }
     }
 
     public void outPutResult(Double output) {
         EditText outputText = (EditText) findViewById(R.id.output);
         outputText.setText(Double.toString(output));
+
+        memory.push(output);
     }
 
-    public Double stringToInt(String str) {
+    public Double stringToDouble(String str) {
         try {
             return new Double(str);
         } catch (NumberFormatException e) {
             Toast.makeText(this, "Неккоректные данные", Toast.LENGTH_LONG).show();
 
             return 0.0;
+        }
+    }
+
+    public void addMemory() {
+        EditText output = (EditText) findViewById(R.id.output);
+        Double result;
+        try {
+            result = new Double(output.getText().toString());
+            memory.push(result);
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "Нечего запоминать", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void popMemory() {
+        EditText firstText = (EditText) findViewById(R.id.editText2);
+        if (memory.empty()) {
+            Toast.makeText(this, "Память калькулятора пуста", Toast.LENGTH_LONG).show();
+        } else {
+            firstText.setText(Double.toString(memory.pop()));
         }
     }
 
@@ -76,29 +110,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void multipliaction() {
-        Double firstArgument = this.stringToInt(this.getFirstValue());
-        Double secondArgument = this.stringToInt(this.getSecondValue());
+        Double firstArgument = this.stringToDouble(this.getFirstValue());
+        Double secondArgument = this.stringToDouble(this.getSecondValue());
 
         outPutResult(firstArgument * secondArgument);
     }
 
     private void substraction() {
-        Double firstArgument = this.stringToInt(this.getFirstValue());
-        Double secondArgument = this.stringToInt(this.getSecondValue());
+        Double firstArgument = this.stringToDouble(this.getFirstValue());
+        Double secondArgument = this.stringToDouble(this.getSecondValue());
 
         outPutResult(firstArgument - secondArgument);
     }
 
     private void addition() {
-        Double firstArgument = this.stringToInt(this.getFirstValue());
-        Double secondArgument = this.stringToInt(this.getSecondValue());
+        Double firstArgument = this.stringToDouble(this.getFirstValue());
+        Double secondArgument = this.stringToDouble(this.getSecondValue());
 
         outPutResult(firstArgument + secondArgument);
     }
 
     private void division() {
-        Double firstArgument = this.stringToInt(this.getFirstValue());
-        Double secondArgument = this.stringToInt(this.getSecondValue());
+        Double firstArgument = this.stringToDouble(this.getFirstValue());
+        Double secondArgument = this.stringToDouble(this.getSecondValue());
 
         if (secondArgument == 0) {
             Toast.makeText(this, "Деление на 0 невозможно", Toast.LENGTH_LONG).show();
